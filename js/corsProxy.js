cceccb25a933284aa7e2e2f68d6e6ec99dfb6c7b
jeducubillosn <45133608+jeducubillosn@users.noreply.github.com>
2024-06-27 13:17:38 -0500

    Update corsProxy.js

--- js/corsProxy.js
+++ js/corsProxy.js
@@ -1,5 +1,5 @@
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const targetUrl = 'https://script.google.com/macros/s/AKfycbzLnMdef9B39_7F2zZNRV-mElOwMXWc-2aAeaMTXv3q_OHsyFhUp3x6Gj-NKXI-3-mO/exec';
+const targetUrl = 'https://script.google.com/macros/s/AKfycbwe25mjvSX-X-HfKlxypuBHUxBrXDEwpj2HnGKDcE9HBI_Vd67GT17H8huS3yDcOs0x/exec';
 
 async function enviarDatosConProxy(id, pregunta, opcionSeleccionada) {
     const url = proxyUrl + targetUrl;
