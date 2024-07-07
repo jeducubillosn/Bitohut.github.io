1457591128ac144dfdeb6505a654cb9f888eedd2
jeducubillosn <45133608+jeducubillosn@users.noreply.github.com>
2024-07-07 13:01:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,25 +12,21 @@ async function cargarPreguntas() {
     }
 }
 
-function setBodyClassByLevel(level) {
-    const body = document.body;
-    body.className = ''; // Elimina todas las clases anteriores
-    body.classList.add(`nivel-${level}`);
-}
-
 function mostrarPregunta(pregunta) {
     const tituloPregunta = document.getElementById('titulo-pregunta');
     const contenedorOpciones = document.getElementById('contenedor-opciones');
 
-    if (!pregunta || !pregunta.opciones) {
-        console.error('Pregunta u opciones no encontradas:', pregunta);
-        return;
-    }
-
+    // Determinar el nivel de la pregunta
     const niveles = pregunta.id.split('-').filter(n => n !== '0').length;
-    setBodyClassByLevel(niveles);
+    const nivelClase = `nivel-${niveles}`;
+
+    // Eliminar clases previas y agregar la clase del nivel actual al body
+    document.body.className = '';
+    document.body.classList.add(nivelClase);
 
     tituloPregunta.textContent = pregunta.pregunta;
+
+    // Limpiar opciones anteriores si las hubiera
     contenedorOpciones.innerHTML = '';
 
     if (Array.isArray(pregunta.opciones)) {
@@ -56,13 +52,10 @@ async function manejarRespuesta(preguntaActual, opcionSeleccionada) {
 
     if (siguientePregunta) {
         mostrarPregunta(siguientePregunta);
-        const progreso = calcularProgreso(siguientePreguntaID);
-        updateProgress(progreso);
     } else if (siguientePreguntaID === '0-0-0-0') {
         const preguntaInicial = await obtenerPregunta('0-0-0-0');
         if (preguntaInicial) {
             mostrarPregunta(preguntaInicial);
-            updateProgress(0);
         } else {
             console.error('No se pudo cargar la pregunta inicial');
         }
@@ -75,6 +68,7 @@ function obtenerSiguientePreguntaID(idActual, opcionSeleccionada) {
     const niveles = idActual.split('-').map(Number);
 
     if (niveles[3] !== 0) {
+        // Cuando está en el nivel 5, volver al nivel 1
         return '0-0-0-0';
     } else if (niveles[0] === 0) {
         niveles[0] = opcionSeleccionada;
@@ -98,11 +92,6 @@ async function obtenerPregunta(preguntaID) {
     }
     console.error('Pregunta no encontrada:', preguntaID);
     return null;
-}
-
-function calcularProgreso(preguntaID) {
-    const niveles = preguntaID.split('-').filter(n => n !== '0').length;
-    return (niveles / 4) * 100;
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
