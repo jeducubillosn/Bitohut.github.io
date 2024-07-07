128d69aef80e42e6adaaa3c6eb42cf51bea98417
jeducubillosn <45133608+jeducubillosn@users.noreply.github.com>
2024-07-07 13:27:00 -0500

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,11 +1,11 @@
 function updateProgress(progressPercentage) {
     const progressBar = document.querySelector('.progress');
-    progressBar.style.width = ${progressPercentage}%;
+    progressBar.style.width = `${progressPercentage}%`; // Corrección aquí
 }
 
 function setBodyClassByLevel(level) {
     document.body.className = '';
-    document.body.classList.add(nivel-${level});
+    document.body.classList.add(`nivel-${level}`); // Corrección aquí
 }
 
 function initializeUI() {
